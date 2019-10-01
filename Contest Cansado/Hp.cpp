e54ffe68dc004f7522c6596d9e6ef6e53e651aae
Caio Fernandes <caiovf13@gmail.com>
2019-10-01 09:15:19 -0300

    Add contest files

---
+++ Contest Cansado/Hp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int k;
+    cin>>i;
+    int month[12];
+    for(int i=0;i<12;i++){
+        cin>>month[i];
+    }
+
+    sort(month);
+
+
+}
